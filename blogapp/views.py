ab70a6e123ad977bd6336d5a3090fefd4fc83863
Dhanush b Mohan <65810261+dhanushbmohan7@users.noreply.github.com>
2020-12-23 09:21:03 +0530

    Update views.py

--- blogapp/views.py
+++ blogapp/views.py
@@ -7,6 +7,7 @@ def index(request):
     data=profiles.objects.all()
     total_recieved=profiles.objects.all().aggregate(Sum('amount_paid'))
     total_pending=profiles.objects.all().aggregate(Sum('amount_balance'))
+    balance=total_recieved['amount_paid__sum']-500
 
 
-    return render(request,'index.html',{'data':data,'total_recieved':total_recieved['amount_paid__sum'],'total_pending':total_pending['amount_balance__sum']})
+    return render(request,'index.html',{'data':data,'total_recieved':total_recieved['amount_paid__sum'],'total_pending':total_pending['amount_balance__sum'],'balance':balance})
